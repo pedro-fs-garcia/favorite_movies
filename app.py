17fe88770bfd64ea1e944e02a44ea3cc5a9c8248
pedro <you@example.com>
2024-03-13 17:09:18 -0300

    message

--- app.py
+++ app.py
@@ -3,8 +3,22 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home_page():
     return render_template("index.html")
+
+@app.route("/directors")
+def directors():
+    return render_template("directors.html")
+
+
+@app.route("/all_films")
+def all_films():
+    return render_template("all_films.html")
+
+@app.route("/my_attempts")
+def my_attempts():
+    return render_template("my_attempts.html")
 
 
 if __name__ == '__main__':
