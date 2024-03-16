6d1987c042030400e4627e644288fa3f915ecbac
pedro <you@example.com>
2024-03-16 14:35:08 -0300

    use of python script to build the "all films" page

--- app.py
+++ app.py
@@ -13,27 +13,16 @@ def directors():
     return render_template("directors.html")
 
 
-@app.route("/all_films")
-def all_films():
-    return render_template("all_films.html")
-
 @app.route("/my_attempts")
 def my_attempts():
     return render_template("my_attempts.html")
 
 
-@app.route("/directors/<name>")
-def redirect_page(name):
-    return render_template(f"/directors/{name}.html")
-
-
-@app.route("/all_films/<film>")
-def redirect_film(film):
-    return render_template(f"/all_films/{film}.html")
-
-
-
-
+@app.route("/all_films")
+def redirect_film():
+    with open("./templates/directors/films.txt", "r") as file:
+        for line in file: films = line.split()
+    return render_template("all_films.html", films=films)
 
 
 if __name__ == '__main__':
--- templates/directors/create_pages.py
+++ templates/directors/create_pages.py
@@ -1,23 +1,35 @@
-names = open("names.txt", "r")
-for i in names:
-    d = i
+# names = open("names.txt", "r")
+# for i in names:
+#     d = i
 
-print(d)
+# print(d)
 
 # for name in d.split():
 #     with open(f"{name}.html", "w") as file:
 #         file.write("{% extends 'base.html' %}\n{% block title %}" + f"{name.replace('_', ' ')}" + "{% endblock %}\n{% block page_title %}My Favorite Directors{% endblock %}\n{% block header_image %}"+ f"<header class='header_image' style='background-image:url(/static/images/{name}.jpg); background-position: center -160px'></header>" +"{% endblock %}\n{% block content %}\n"+f"<h2>{name.replace('_', ' ').title()}</h2>" + "{% endblock %}")
 
-for name in d.split():
-    with open("collapsable.txt", "a") as file:
-        file.write(f'''<details>
-        <summary><h3 style="margin:2px">{name.replace("_", " ").title()}</h3>
-            <div class="director_summary">
-            <div>
-                <img class="director_image" src="/static/images/{name}.jpg">
-            </div>
-            <div style="margin:auto 15px;">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae repellendus perspiciatis natus reiciendis eaque repudiandae consectetur corrupti sequi blanditiis maiores, necessitatibus magnam, asperiores recusandae, aliquam esse? Quaerat quos aperiam reiciendis.</div>
-        </div>
-        </summary>
-        <div class="movie_posters">his main movies</div>
-    </details><br>\n\n''')
+# for name in d.split():
+#     with open("collapsable.txt", "a") as file:
+#         file.write(f'''<details>
+#         <summary><h3 style="margin:2px">{name.replace("_", " ").title()}</h3>
+#             <div class="director_summary">
+#             <div>
+#                 <img class="director_image" src="/static/images/{name}.jpg">
+#             </div>
+#             <div style="margin:auto 15px;">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae repellendus perspiciatis natus reiciendis eaque repudiandae consectetur corrupti sequi blanditiis maiores, necessitatibus magnam, asperiores recusandae, aliquam esse? Quaerat quos aperiam reiciendis.</div>
+#         </div>
+#         </summary>
+#         <div class="movie_posters">his main movies</div>
+#     </details><br>\n\n''')
+
+
+films = open("films.txt")
+for i in films:
+    f = i
+
+for film in f.split():
+        with open("film_list.txt", "a") as file:
+                file.write(f'''
+<figure style="text-align: center;"><a href="" target="_blank">
+    <img class="film_poster" src="/static/images/{film}.jpg"><br>{film.replace('_',' ').title()}
+</a></figure>\n''')
