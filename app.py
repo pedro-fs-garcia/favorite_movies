95fd403f1ccb96654ccb92b30bf3a9ed967fac96
Pedro Garcia <158316886+pedro-fs-garcia@users.noreply.github.com>
2024-05-26 14:25:12 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,31 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 import json
 
 app = Flask(__name__)
 
+perguntas = json.load(open("./templates/questions.json", encoding = "UTF-8"))
 
 @app.route("/")
-@app.route("/home")
 def home_page():
-    return render_template("index.html")
+    return redirect ("/home")
 
 
-@app.route("/directors")
-def directors():
-    directors = json.load(open("./templates/directors_films.json"))
-    return render_template("directors.html", directors = directors)
+@app.route("/<name>")
+def get_page(name):
+    directors = json.load(open("./templates/directors_films.json", encoding="UTF-8"))
+    perguntas = json.load(open("./templates/questions.json", encoding="UTF-8"))
+    if name == "home":
+        return render_template(f"index.html", directors = directors, perguntas=perguntas)
+
+    return render_template(f"{name}.html", directors = directors, perguntas=perguntas)
 
 
-@app.route("/all_films")
-def redirect_film():
-    directors = json.load(open("./templates/directors_films.json"))
-    return render_template("all_films.html", directors=directors)
+@app.route("/get_answer", methods=["POST", "GET"])
+def get_answer():
+    respostas={}
+    acertos = 0
+    for i in range(1, len(perguntas)+1):
+        try:
+            respostas[f"answer{i}"] = request.form[f"answer{i}"]
+        except KeyError:
+            respostas[f"answer{i}"] = ""
+    
+    for i in range(1, len(perguntas)+1):
+        if perguntas[str(i)][5] == respostas[f"answer{i}"]:
+            acertos += 1
+    
+    erros = len(perguntas) - acertos
+    porcentagem = f"{(acertos/len(perguntas) * 100):.2f}%"
+
+    return render_template("result.html", acertos=acertos, erros=erros, porcentagem=porcentagem)
 
 
-@app.route("/my_attempts")
-def my_attempts():
-    return render_template("my_attempts.html")
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000', debug=True)
